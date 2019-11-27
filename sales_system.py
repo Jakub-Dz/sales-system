c4ff0347340007c9aa5e363381320eb62da69474
Jakub-Dz <dziurdz.jakub@gmail.com>
2019-11-27 16:44:40 +0000

    stock json added as prescribed

--- sales_system.py
+++ sales_system.py
@@ -3,5 +3,11 @@ This sales system is for customer use, not company use.
 The company only has one customer.
 """
 from random import randint
+import json
 
 print(randint(0, 5))
+
+with open("stock.json", "r") as json_file:
+    data = json.load(json_file)
+
+print(json.dumps(data))
