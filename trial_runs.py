cc66ab59947ccea7636cd09b5e34d07deaee4656
Jakub-Dz <kubakonto@o2.pl>
2019-12-02 18:34:20 +0000

    Stock class added

--- trial_runs.py
+++ trial_runs.py
@@ -13,10 +13,25 @@ class Item:
         self.quantity = quantity
 
 
-class Stock(object):
+class Stock:
 
-    def __init__(self, d):
-        self.__dict__ = d
+    def __init__(self):
+        with open("stock.json", "r") as stock_file:
+            self.stock_dict = json.load(stock_file)
+
+    def display(self, item_name="all"):
+        if item_name == "all":
+            for item in self.stock_dict:
+                print(item, self.stock_dict[item]["price"], self.stock_dict[item]["quantity"])
+        else:
+            print(item_name, self.stock_dict[item_name]["price"], self.stock_dict[item_name]["quantity"])
+
+    def remove(self, item_name, quantity):
+        self.stock_dict[item_name]["quantity"] -= quantity
+
+    def save(self):
+        with open("stock.json", "w") as stock_file:
+            json.dump(self.stock_dict, stock_file, indent=3)
 
 
 class User:
@@ -41,6 +56,10 @@ class Transaction:
     def user_details(self):
         return self.user.user_name, self.user.wallet
 
+    def stock(self):
+        with open("stock.json", "r") as stock_file:
+            self.stock_data = json.load(stock_file)
+
 
 cost = 2000
 user_1 = User("Basil Fawlty", 1267.56)
@@ -52,8 +71,8 @@ else:
 transaction_1 = Transaction(user_1)
 print(transaction_1.user_details())
 
-with open("stock.json", "r") as stock_file:
-    stock_data = json.load(stock_file)
-
-for item in stock_data:
-    print(stock_data[item])
+stock = Stock()
+stock.display()
+stock.remove("Bookcase", 0)
+stock.display("Bookcase")
+stock.save()
