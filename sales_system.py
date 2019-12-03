a7253ef4bf6cfce17761609f9f70bdd4e841c4f4
Jakub-Dz <kubakonto@o2.pl>
2019-12-03 21:59:19 +0000

    removed display methods

--- sales_system.py
+++ sales_system.py
@@ -3,6 +3,7 @@ This sales system is for customer use, not company use.
 The company only has one customer.
 """
 import json
+import tkinter as tk
 
 
 class Item:
@@ -19,13 +20,6 @@ class Stock:
         with open("stock.json", "r") as stock_file:
             self.stock_dict = json.load(stock_file)
 
-    def display(self, item_name="all"):
-        if item_name == "all":
-            for item in self.stock_dict:
-                print(item, self.stock_dict[item]["price"], self.stock_dict[item]["quantity"])
-        else:
-            print(item_name, self.stock_dict[item_name]["price"], self.stock_dict[item_name]["quantity"])
-
     def remove(self, item_name, quantity):
         self.stock_dict[item_name]["quantity"] -= quantity
 
@@ -36,8 +30,8 @@ class Stock:
 
 class User:
 
-    def __init__(self, user_name, wallet):
-        self.user_name = user_name
+    def __init__(self, name, wallet):
+        self.name = name
         self.wallet = wallet
 
     def check_balance(self, checkout_cost):
@@ -45,7 +39,6 @@ class User:
 
     def update_wallet(self, checkout_cost):
         self.wallet -= checkout_cost
-        return self.wallet
 
 
 class Basket:
@@ -62,27 +55,37 @@ class Basket:
         self.value -= self.items[item_name][1]
         del self.items[item_name]
 
-    def display(self):
-        print(self.items)
-        print(f'Total basket value: {self.value:.2f}.')
-
 
 class Transaction:
 
-    def __init__(self, user):
-        self.user = user
+    def __init__(self, name):
+        self.name = name
 
     def user_details(self):
-        return self.user.user_name, self.user.wallet
+        pass
+        # return self.user.user_name, self.user.wallet
     # if self.stock_dict[item_name]["quantity"] - quantity >= 0:
 
 
+class GUI:
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+
+user = User("Basil Fawlty", 1087.65)  # Hardcoded values as per project's spec
 stock = Stock()
 basket = Basket()
+gui = GUI()  # https://stackoverflow.com/questions/17466561/best-way-to-structure-a-tkinter-application
 
 # Testing only
-basket.add_item("Sofa", 4)
-basket.add_item("Bed", 2)
-basket.display()
-basket.remove_item("Bed")
-basket.display()
+print(f'Username: {user.name}\nFunds available: £{user.wallet:.2f}')
+print(f'Number of items in the basket: {len(basket.items)}\nTotal value: £{basket.value:.2f}')
+for item in stock.stock_dict:
+    print(f'{item} | Price: £{stock.stock_dict[item]["price"]} | Qty: {stock.stock_dict[item]["quantity"]}')
+
+basket.add_item("Bed", 1)
+stock.remove("Bed", 1)
+print(f'Number of items in the basket: {len(basket.items)}\nTotal value: £{basket.value:.2f}')
+for item in stock.stock_dict:
+    print(f'{item} | Price: £{stock.stock_dict[item]["price"]} | Qty: {stock.stock_dict[item]["quantity"]}')
