35d8688aeea566f9e764342916edce8f4df01221
Jakub-Dz <kubakonto@o2.pl>
2019-12-07 22:06:33 +0000

    Further tkinter updates, basket remove items TBC

--- sales_system.py
+++ sales_system.py
@@ -49,11 +49,17 @@ class Basket:
         self.items = {}
 
     def add_item(self, item_name, quantity):
-        self.items[item_name] = (quantity, quantity * stock.stock_dict[item_name]["price"])  # requires stock = Stock()
-        self.value += self.items[item_name][1]
+        if item_name in self.items:
+            new_quantity = self.items[item_name][1] + quantity
+        else:
+            new_quantity = quantity
 
+        self.items[item_name] = (new_quantity * stock.stock_dict[item_name]["price"], new_quantity)
+        self.value += self.items[item_name][0]
+
+    # Removing item by type completely, quantity edits not implemented
     def remove_item(self, item_name):
-        self.value -= self.items[item_name][1]
+        self.value -= self.items[item_name][0]
         del self.items[item_name]
 
 
@@ -74,44 +80,52 @@ class SalesGUI:
         master.title("Sales System")
         master.resizable(False, False)
         master.option_add("*Font", "consolas")
+        # http://www.science.smith.edu/dftwiki/images/3/3d/TkInterColorCharts.png
 
-        window_size = "600x400"
-        window_xpos = int(root.winfo_screenwidth()/2 - root.winfo_reqwidth())
+        window_size = "800x400"
+        window_xpos = int(root.winfo_screenwidth()/2 - 2 * root.winfo_reqwidth())
         window_ypos = int(root.winfo_screenheight()/2 - root.winfo_reqheight())
 
         master.geometry(f'{window_size}+{window_xpos}+{window_ypos}')
+        master.configure(bg="gray95")
 
-        self.label = tk.Label(master, text="This is our first GUI!")
-        self.label.grid(column=0, row=0)
+        self.stock_control = []
+        self.stock_listbox(stock.stock_dict)
 
-        #  create main containers
-        self.top_left_frame = tk.Frame(master, bg="cyan", width=400, height=100, pady=3)
-        self.top_right_frame = tk.Frame(master, bg="red", width=200, height=100, pady=3)
-        self.bottom_left_frame = tk.Frame(master, bg="green", width=400, height=300, pady=3)
-        self.bottom_right_frame = tk.Frame(master, bg="blue", width=200, height=300, pady=3)
+        self.basket_control = []
+        self.basket_listbox(basket.items)
 
-        # layout all of the main containers
-        self.top_left_frame.grid(row=0, column=0, sticky="w")
-        self.top_right_frame.grid(row=0, column=1, sticky="e")
-        self.bottom_left_frame.grid(row=1, column=0, sticky="sw")
-        self.bottom_right_frame.grid(row=1, column=1, sticky="se")
+        self.var = StringVar()
+        self.var.set(f'Basket value: £{basket.value}')
+
+        # GUI Labels
+        self.stock_label = tk.Label(master, text="Furniture catalogue:")
+        self.stock_label.place(x=180, y=55)
+
+        self.basket_top_label = tk.Label(master, text="Your basket:")
+        self.basket_top_label.place(x=580, y=55)
+
+        self.basket_total_label = tk.Label(master, textvariable=self.var)
+        self.basket_total_label.place(x=535, y=282)
+        # GUI Buttons
+
+        self.remove_button = tk.Button(master, text="Remove Item", command=self.remove)
+        self.remove_button.config(width=25, height=1, bg="red")
+        self.remove_button.place(x=520, y=320)
 
         self.greet_button = tk.Button(master, text="    BUY    ", command=self.greet)
-        self.greet_button.grid(column=0, row=0)
+        self.greet_button.place(x=0, y=0)
 
         self.close_button = tk.Button(master, text="CANCEL", command=master.quit)
-        self.close_button.grid(column=0, row=1, columnspan=1)
+        self.close_button.place(x=0, y=0)
 
         self.close_test = tk.Button(master, text="Test", command=self.test)
-        self.close_test.grid(column=1, row=1, columnspan=1)
-        self.close_test.config(width=5, height=5)
+        self.close_test.place(x=0, y=0)
+        self.close_test.config(width=1, height=1)
 
         #  listbox https://stackoverflow.com/questions/15672552/python-tkinter-listbox-get-active-method
         #  http://zetcode.com/tkinter/widgets/
         #  https://stackoverflow.com/questions/34276663/tkinter-gui-layout-using-frames-and-grid
-
-        self.stock_listbox(stock.stock_dict)
-        self.var = StringVar()
 
     def greet(self):
         print("Greetings!")
@@ -119,49 +133,65 @@ class SalesGUI:
     def test(self):
         tk.messagebox.showinfo("Test", "Test2")
 
-    def stock_listbox(self, stock_list):
-        #stock_list = ["test1", "test2", "test3"]
-        stock_lb = tk.Listbox(self.master)
+    def stock_listbox(self, items_dict):
+        items_lb = tk.Listbox(self.master)
 
-        for item in stock_list:
-            stock_lb.insert(END, f'{item:<16} | Price: £{stock.stock_dict[item]["price"]:<6} | Available: {stock.stock_dict[item]["quantity"]}')
+        for item in items_dict:
+            text_item = f'{item:16}'
+            text_price = f'Price: £{items_dict[item]["price"]:<6}'
+            text_qty = f'Available: {items_dict[item]["quantity"]}'
+            items_lb.insert(END, f'{text_item} | {text_price} | {text_qty}')
+            self.stock_control.append(item)
 
-        stock_lb.bind("<<ListboxSelect>>", self.on_select)
-        stock_lb.config(width=49)
-        stock_lb.place(x=10, y=10)
+        items_lb.bind("<<ListboxSelect>>", self.stock_select)
+        items_lb.config(width=49)
+        items_lb.place(x=40, y=80)
 
-    def on_select(self, val):
+    def basket_listbox(self, basket_list):
+        basket_lb = tk.Listbox(self.master)
+
+        for item in basket_list:
+
+            text_item = f'{item[:11]:11}'
+            text_price = f'£{basket_list[item][0]:<6}'
+            text_qty = f'{basket_list[item][1]}'
+            basket_lb.insert(END, f'{text_item} | {text_price} | {text_qty}')
+
+            self.basket_control.append(item)
+
+        basket_lb.bind("<<ListboxSelect>>", self.basket_select)
+        basket_lb.config(width=26)
+        basket_lb.place(x=520, y=80)
+
+    def stock_select(self, val):
         sender = val.widget
-        print(sender)
         idx = sender.curselection()
-        print(idx)
-        value = sender.get(idx)
-        print(value)
+        tk.messagebox.showinfo("Test", self.stock_control[(idx[0])])
 
-        self.var.set(value)
-        print(self.var)
-        tk.messagebox.showinfo("Test", self.var.get())
+    def basket_select(self, val):
+        sender = val.widget
+        idx = sender.curselection()
+        tk.messagebox.showinfo("Test", self.basket_control[(idx[0])])
+        # self.selected_item = self.basket_control[(idx[0])]
+
+    def remove(self):
+        pass
+        # basket.remove_item(self.selected_item)
 
 
 user = User("Basil Fawlty", 1087.65)  # Hardcoded values as per project's spec
 stock = Stock()
 basket = Basket()
 
-# Testing only
-# print(f'Username: {user.name}\nFunds available: £{user.wallet:.2f}')
-# print(f'Number of items in the basket: {len(basket.items)}\nTotal value: £{basket.value:.2f}')
-# for item in stock.stock_dict:
-#     print(f'{item} | Price: £{stock.stock_dict[item]["price"]} | Qty: {stock.stock_dict[item]["quantity"]}')
-#
-# basket.add_item("Bed", 1)
-# stock.remove("Bed", 1)
-# print(f'Number of items in the basket: {len(basket.items)}\nTotal value: £{basket.value:.2f}')
-# for item in stock.stock_dict:
-#     print(f'{item} | Price: £{stock.stock_dict[item]["price"]} | Qty: {stock.stock_dict[item]["quantity"]}')
+basket.add_item("Bed", 1)
+basket.add_item("Bed", 1)
+basket.add_item("Chest of Drawers", 1)
+stock.remove("Bed", 1)
+stock.remove("Chest of Drawers", 1)
 
-a = "Test"
-b = f"{a:<10}"
-print(len(a), len(b))
 root = tk.Tk()
 my_gui = SalesGUI(root)
-root.mainloop()
+# root.mainloop()
+while True:
+    root.update()
+
